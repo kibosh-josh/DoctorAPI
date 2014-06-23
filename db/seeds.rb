71997b67c0bc03d1eeecfc06f8badbe9f9b74a65
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-06-22 23:15:35 -0700

    added database through scraping

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 
-browser = Watir::Browser.start "https://www.blueshieldca.com/fap/app/search.html"
+browser = Watir::Browser.start "https://www.thesiteIscraped.com"
 browser.link(:text, "select").click
-browser.select_list(:id, "medical").when_present.select("2014 Individual and Family PPO Plans (including Covered California)")
-browser.select_list(:id, "medSubPaln").when_present.select("Basic PPO for HSA (Bronze level)")
-browser.button(:value,"setplan").click
+browser.select_list(:id, "someId").when_present.select("2014 Individual and Family PPO Plans (including Covered California)")
+browser.select_list(:id, "someID").when_present.select("Basic PPO for HSA (Bronze level)")
+browser.button(:value,"anothervalue").click
 browser.text_field(:id, "location").set("94110")
 browser.button(:value,"findNow").click
 until browser.select_list(:id, "radiusid").exists?
@@ -37,85 +37,89 @@ until y == 5000
   end
 end
 
-# browser = Watir::Browser.start "https://www.anthem.com/health-insurance/provider-directory/searchcriteria?qs=*5MFKC18jLwG2/gYGCqVozg==&brand=abc"
-#   until
-#     browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard6_SearchWizard5_ZipCityStateRadius").exists?
-#     sleep 1
-#   end
-# browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard6_SearchWizard5_ZipCityStateRadius").when_present.select("10")
-# browser.text_field(:id, "ctl00_MainContent_maincontent_SearchWizard6_SearchWizard5_txtZipCityState").set("94110")
-# browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplanstate").select("California")
-# browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplans").select("Pathway X - PPO / Individual via Exchange")
-# browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplan").select("Bronze DirectAccess")
-# browser.button(:id, "btnSearch").click
+browser = Watir::Browser.start "https://www.anothersitescraped.com"
+sleep 5
+browser.select_list(:id, "aRidculousID").when_present.select("10")
+browser.text_field(:id, "ctl00_MainContent_maincontent_SearchWizard6_SearchWizard5_txtZipCityState").set("94110")
+browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplanstate").select("California")
+browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplans").select("Pathway X - PPO / Individual via Exchange")
+browser.select_list(:id, "ctl00_MainContent_maincontent_SearchWizard7_ddlplan").select("Bronze DirectAccess")
+browser.button(:id, "btnSearch").click
 
-# y = 1
-# x = 1
-# until y == 100
-#   if x % 60 == 0 || y % 60 == 0
-#     x = 1
-#   end
-#   if browser.a(:class, "fsrCloseBtn").exists?
-#     browser.a(:class, "fsrCloseBtn").click
-#   end
-#   if y % 20 == 0 || x % 20 == 0
-#     browser.a(:id, "tablePagination_nextPage").click
-#   end
-#   until
-#     browser.a(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lnkName").exists?
-#     sleep 1
-#   end
-#   if browser.a(:class, "fsrCloseBtn").exists?
-#     browser.a(:class, "fsrCloseBtn").click
-#   end
+y = 1
+x = 0
+until y == 3000
+  if y % 60 == 0
+    x = 0
+  end
+  if browser.a(:class, "fsrCloseBtn").exists?
+    browser.a(:class, "fsrCloseBtn").click
+  end
+  if y % 20 == 0
+    browser.a(:id, "tablePagination_nextPage").click
+    sleep 5
+  end
+  until
+    browser.a(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lnkName").exists?
+    sleep 1
+  end
+  if browser.a(:class, "fsrCloseBtn").exists?
+    browser.a(:class, "fsrCloseBtn").click
+  end
 
-#   processed_name = browser.a(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lnkName").text
-#   if processed_name
-#     name = processed_name.split(" ")[1].capitalize + ", " + processed_name.split(" ")[0].capitalize + " MD"
-#   end
-#   specialty = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_specialtyList_ctrl0_lblSpecialty").text.split(",").join
-#   phone = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblPhoneNumber").text
-#   address = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblAddress").text.split(",").join
-#   city = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblCity").text.split(",").join
-#   zip_code = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblZipCode").text
+  if 
+    browser.a(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lnkName").exists? == true
+    processed_name = browser.a(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lnkName").text
+  end
+  if processed_name != ""
+    name = processed_name.split(" ")[1].capitalize + ", " + processed_name.split(" ")[0].capitalize + " MD"
+  else
+    name = ""
+  end
+  specialty = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_specialtyList_ctrl0_lblSpecialty").text.split(",").join
+  phone = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblPhoneNumber").text
+  address = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblAddress").text.split(",").join
+  city = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblCity").text.split(",").join
+  zip_code = browser.span(:id, "ctl00_ResultsList_ctrl" + x.to_s + "_lblZipCode").text
 
-#   Doctor.create!(name: name, specialty: specialty, phone: phone, address: address, city: city, zip_code: zip_code, insurance_plan: "blue_cross_bronze_60_PPO")
-  
-#   y += 1
-#   x += 1
-# end
-# doctors_chinese_community = JSON.parse(File.read(chinese_community_health))
+  Doctor.find_or_create_by(name: name, specialty: specialty, phone: phone, address: address, city: city, zip_code: zip_code, insurance_plan: "blue_cross_bronze_60_PPO")
+  p x
+  p y
+  y += 1
+  x += 1
+end
+doctors_chinese_community = JSON.parse(File.read(chinese_community_health))
 
-# doctors_blue_cross = JSON.parse(File.read(anthem_blue_cross))
+doctors_blue_cross = JSON.parse(File.read(anthem_blue_cross))
 
-# doctors_kaiser = JSON.parse(File.read(kaiser))
+doctors_kaiser = JSON.parse(File.read(kaiser))
 
-# doctors_blue_shield = JSON.parse(File.read(blue_shield))
+doctors_blue_shield = JSON.parse(File.read(blue_shield))
 
-# doctors_chinese_community["data"].each do |doctor|
-#   last_name = doctor["name"].join("").split(",")[0].strip.titleize
-#   first_name = doctor["name"].join("").split(",")[1].strip.titleize
-#   address = doctor["address"].join
-#   Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "cchp_bronze_60_HMO")
-# end
+doctors_chinese_community["data"].each do |doctor|
+  last_name = doctor["name"].join("").split(",")[0].strip.titleize
+  first_name = doctor["name"].join("").split(",")[1].strip.titleize
+  address = doctor["address"].join
+  Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "cchp_bronze_60_HMO")
+end
 
-# doctors_blue_cross["data"].each do |doctor|
-#   last_name = doctor["name"].join("").split(",")[0].strip.titleize
-#   first_name = doctor["name"].join("").split(",")[1].strip.titleize
-#   address = doctor["address"].join
-#   Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "blue_cross_bronze_60_PPO")
-# end
+doctors_blue_cross["data"].each do |doctor|
+  last_name = doctor["name"].join("").split(",")[0].strip.titleize
+  first_name = doctor["name"].join("").split(",")[1].strip.titleize
+  address = doctor["address"].join
+  Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "blue_cross_bronze_60_PPO")
+end
 
-# doctors_kaiser["data"].each do |doctor|
-#   last_name = doctor["name"].join("").split(",")[0].strip.titleize
-#   first_name = doctor["name"].join("").split(",")[1].strip.titleize
-#   address = doctor["address"].join
-#   Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "kaiser_bronze_HMO_4500_40")
-# end
+doctors_kaiser["data"].each do |doctor|
+  last_name = doctor["name"].join("").split(",")[0].strip.titleize
+  first_name = doctor["name"].join("").split(",")[1].strip.titleize
+  address = doctor["address"].join
+  Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "kaiser_bronze_HMO_4500_40")
+end
 
-# doctors_blue_shield["data"].each do |doctor|
-#   last_name = doctor["name"].join("").split(",")[0].strip.titleize
-#   first_name = doctor["name"].join("").split(",")[1].strip.titleize
-#   address = doctor["address"].join
-#   Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "blue_shield_bronze_60_PPO")
-# end
+doctors_blue_shield["data"].each do |doctor|
+  last_name = doctor["name"].join("").split(",")[0].strip.titleize
+  first_name = doctor["name"].join("").split(",")[1].strip.titleize
+  address = doctor["address"].join
+  Doctor.create!(address: address, first_name: first_name, last_name: last_name, insurance_plan: "blue_shield_bronze_60_PPO")
+end
