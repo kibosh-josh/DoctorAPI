df71d19d757a2885dcdeec1175c5489abb856bf4
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-06-22 19:19:25 -0700

    added routes, deleted some comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,17 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  namespace :api do
+    namespace :v1 do
+      get '/doctors' => 'doctors#index'
+      get '/cchp' => 'doctors#cchp'
+      get '/blue_cross' => 'doctors#blue_cross'
+      get '/blue_shield' => 'doctors#blue_shield'
+      get '/kaiser' => 'doctors#kaiser'
+    end
+  end
 end
+#             Prefix Verb URI Pattern                   Controller#Action
+#     api_v1_doctors GET  /api/v1/doctors(.:format)     api/v1/doctors#index {:format=>"json"}
+#        api_v1_cchp GET  /api/v1/cchp(.:format)        api/v1/doctors#cchp {:format=>"json"}
+#  api_v1_blue_cross GET  /api/v1/blue_cross(.:format)  api/v1/doctors#blue_cross {:format=>"json"}
+# api_v1_blue_shield GET  /api/v1/blue_shield(.:format) api/v1/doctors#blue_shield {:format=>"json"}
+#      api_v1_kaiser GET  /api/v1/kaiser(.:format)      api/v1/doctors#kaiser {:format=>"json"}
