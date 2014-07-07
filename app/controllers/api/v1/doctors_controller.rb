c341aa039bd9761392cc94491f7f676e8a877128
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-07-07 16:29:03 -0700

    got rid of unnecessary instance variables

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -7,97 +7,98 @@ module Api
       respond_to :json
       
       def index
+        binding.pry
         if params.include?("name")
           search = params["name"].titleize
-          @doctors = Doctor.where("name like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.where("name like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("specialty")
           search = params["specialty"].titleize
-          @doctors = Doctor.where("specialty like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.where("specialty like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("medicalgroup")
           search = params["medicalgroup"].titleize
-          @doctors = Doctor.where("medical_group like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.where("medical_group like ?", "%#{search}%")
+          respond_with doctors
         else
-          @doctors = Doctor.all
-          respond_with @doctors
+          doctors = Doctor.all
+          respond_with doctors
         end
       end
 
       def cchp
         if params.include?("name")
           search = params["name"].titleize
-          @doctors = Doctor.cchp.where("name like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.cchp.where("name like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("specialty")
           search = params["specialty"].titleize
-          @doctors = Doctor.cchp.where("specialty like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.cchp.where("specialty like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("medicalgroup")
           search = params["medicalgroup"].titleize
-          @doctors = Doctor.cchp.where("medical_group like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.cchp.where("medical_group like ?", "%#{search}%")
+          respond_with doctors
         else
-          @doctors = Doctor.cchp
-          respond_with @doctors
+          doctors = Doctor.cchp
+          respond_with doctors
         end
       end
 
       def blue_cross
         if params.include?("name")
           search = params["name"].titleize
-          @doctors = Doctor.blue_cross.where("name like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_cross.where("name like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("specialty")
           search = params["specialty"].titleize
-          @doctors = Doctor.blue_cross.where("specialty like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_cross.where("specialty like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("medicalgroup")
           search = params["medicalgroup"].titleize
-          @doctors = Doctor.blue_cross.where("medical_group like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_cross.where("medical_group like ?", "%#{search}%")
+          respond_with doctors
         else
-          @doctors = Doctor.blue_cross
-          respond_with @doctors
+          doctors = Doctor.blue_cross
+          respond_with doctors
         end
       end
 
       def blue_shield
         if params.include?("name")
           search = params["name"].titleize
-          @doctors = Doctor.blue_shield.where("name like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_shield.where("name like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("specialty")
           search = params["specialty"].titleize
-          @doctors = Doctor.blue_shield.where("specialty like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_shield.where("specialty like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("medicalgroup")
           search = params["medicalgroup"].titleize
-          @doctors = Doctor.blue_shield.where("medical_group like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.blue_shield.where("medical_group like ?", "%#{search}%")
+          respond_with doctors
         else
-          @doctors = Doctor.blue_shield
-          respond_with @doctors
+          doctors = Doctor.blue_shield
+          respond_with doctors
         end
       end
 
       def kaiser
         if params.include?("name")
           search = params["name"].titleize
-          @doctors = Doctor.kaiser.where("name like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.kaiser.where("name like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("specialty")
           search = params["specialty"].titleize
-          @doctors = Doctor.kaiser.where("specialty like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.kaiser.where("specialty like ?", "%#{search}%")
+          respond_with doctors
         elsif params.include?("medicalgroup")
           search = params["medicalgroup"].titleize
-          @doctors = Doctor.kaiser.where("medical_group like ?", "%#{search}%")
-          respond_with @doctors
+          doctors = Doctor.kaiser.where("medical_group like ?", "%#{search}%")
+          respond_with doctors
         else
-          @doctors = Doctor.kaiser
-          respond_with @doctors
+          doctors = Doctor.kaiser
+          respond_with doctors
         end
       end
 
