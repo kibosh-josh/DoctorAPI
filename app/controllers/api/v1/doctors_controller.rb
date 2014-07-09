c2b3bb1356cbebc00e25b9babf56a7ff775588c6
Joshua-Pearson <j_d_pearson@yahoo.com>
2014-07-08 21:52:32 -0700

    adding secrets.yml again

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -140,7 +140,7 @@ module Api
         headers['Access-Control-Allow-Methods'] = 'GET, OPTIONS'
         headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(',')
         headers['Access-Control-Max-Age'] = '1728000'
-      end 
+      end
     end
   end
 end
